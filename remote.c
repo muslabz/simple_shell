515ce70d4311763fa96d88d366fc9efe53033aea
muslabz <ajetunmobimust3@gmail.com>
2022-11-06 18:44:10 -0800

    second com

---
+++ remote.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+/**
+ * controls - Avoid exit with Ctrl C
+ * @sig: Signal number
+ *
+ * Description: Avoid exit with Ctrl C
+ * Return: Nothing
+ */
+void controls(int sig)
+{
+	(void) sig;
+	write(STDOUT_FILENO, "\n$ ", 3);
+}
+
