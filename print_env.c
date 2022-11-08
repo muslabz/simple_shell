968e4c35a8c7ea6ddbe97cbf6e040b1949ea26d1
muslabz <ajetunmobimust3@gmail.com>
2022-11-07 17:47:20 -0800

    f

---
+++ print_env.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+ * print_env - environment variables
+ * @av: command to recibe
+ * @env: all environment variables for the user
+ * Description: prints all environment variables
+ * Return: 1
+ */
+int print_env(char *av, char **env)
+{
+	unsigned int x = 0, length;
+	int status;
+
+	status = _strcmp(av, "env");
+
+	if (status == 0)
+	{
+		x = 0;
+		while (env[x])
+		{
+			length = _strlen(env[x]);
+			write(STDOUT_FILENO, env[x], length);
+			write(STDOUT_FILENO, "\n", 1);
+			++x;
+		}
+	}
+	return (1);
+}
+
+		
+/**
+ * find_env - find env
+ * @av: env to find
+ * @env: all environment variables for the user
+ *
+ * Description: prints environment finded
+ * Return: 1
+ */
+char *find_env(char *av, char **env)
+{
+	int i, x, len;
+	char *temp, *temp2, *token;
+
+	x = 0;
+	len = _strlen(av);
+
+	while (env[x])
+	{
+		temp = env[x];
+		for (i = 0; temp[i] == av[i]; i++)
+			;
+		if (i == len)
+		{
+			token = strtok(env[x], "=");
+			while (token != NULL)
+			{
+				temp2 = token;
+				token = strtok(NULL, "=");
+			}
+			return (temp2);
+		}
+		++x;
+	}
+	return ("");
+}
+
---
+++ way.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * path_ - handle the path
+ * @av: double pointer
+ * @pfid: find the path
+ * @env: environment
+ * @cont: counter
+ * @flag_1: flag
+ * @n: number
+ * Return: a pointer
+ */
+char *path_(char **av, int *pfid, char **env, int *cont, int *flag_1, int *n)
+{
+	int x = 0, x_av, y;
+	char *arv;
+
+	x = _strlen(av[0]), arv = malloc(sizeof(char) * (x + 1));
+	getav(&x_av, av, arv);
+	for (y = 0; env[y] && access(av[0], X_OK) != 0 && *pfid == 0; y++)
+		Upath(av, env[y], arv, x_av, pfid);
+	if (access(av[0], X_OK) == 0)
+		exece(av, n), cont++, *flag_1 = 1, free(arv);
+	return (arv);
+}
+
